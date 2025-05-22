43b8854277cea6643758e43971d5cedd599776ac
KEVIN NOA ROMERO <kevinnoa02@gmail.com>
2025-05-22 00:32:57 -0500

    tabla dinamica con datos del backend con paginacion incorporado

--- src/pages/Paginacion.jsx
+++ src/pages/Paginacion.jsx
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from 'react'
 export default function Paginacion() {
     const [pagina, setPagina] = useState(0);
     const [totalPaginas, setTotalPaginas] = useState();
+    const [totalElementos, setTotalElementos] = useState(10);
     const [arryPagina, setArryPagina] = useState([]);
 
     const leerServicio = async () => {
-        const url = `http://localhost:8080/cantantes/get-pagination?page=${pagina}&size=30`;
+        const url = `http://localhost:8080/cantantes/get-pagination?page=${pagina}&size=${totalElementos}`;
         const response = await fetch(url);
         const data = await response.json();
         setArryPagina(data.content)
@@ -17,24 +18,41 @@ export default function Paginacion() {
     }
     useEffect(() => {
         leerServicio();
-    }, [])
+    }, [pagina, totalElementos])
 
     const avanzarPagina = (e) => {
         e.preventDefault();
-        if (pagina >= totalPaginas) {
-            setPagina(pagina - 1)
+        //0        //5
+        if (pagina < totalPaginas - 1) {
+            setPagina(pagina + 1)
 
         }
     }
     const retrocederPagina = (e) => {
         e.preventDefault();
-        if (pagina  <  totalPaginas) {
-                setPagina(pagina + 1)
+        //5        //0
+        if (pagina > 0) {
+            setPagina(pagina - 1)
 
         }
     }
+    const actualizar = (e) => {
+        e.preventDefault();
+        setPagina(0);
+        setTotalElementos(parseInt(e.target.value));
+    }
     return (
         <section className='overflow-x-auto p-4'>
+            <div className='flex flex-col'>
+                <label >Mostrar datos </label>
+                <select id="" className='w-50' onChange={(e) => actualizar(e)}>
+                    <option value="" disabled selected >Seleccione un opcion</option>
+                    <option value="10" >10</option>
+                    <option value="30" >30</option>
+                    <option value="50" >50</option>
+                    <option value="100" >100</option>
+                </select>
+            </div>
             <table className='min-w-full text-sm text-left text-gray-500 border border-gray-200 rounded-lg overflow-hidden shadow-lg'>
                 <thead className='text-xs text-white uppercase bg-indigo-600'>
                     <tr>
@@ -62,10 +80,12 @@ export default function Paginacion() {
                 </tbody>
             </table>
 
-            <div className='w-screen bg-amber-400 flex justify-center items-center py-2'>
-                <a onClick={(e) => avanzarPagina(e)} href="" className='mx-2 border-2 rounded px-5 text-amber-100 bg-black'>Next</a>
-                <a href="" className='mx-2 border-2 rounded px-5 text-amber-100 bg-black'>{pagina}</a>
+            <div className='w-full bg-amber-400 flex justify-center items-center py-2'>
                 <a onClick={(e) => retrocederPagina(e)} href="" className='mx-2 border-2 rounded px-5 text-amber-100 bg-black'>Atras</a>
+
+                <a href="" className='mx-2 border-2 rounded px-5 text-amber-100 bg-black'>{pagina + 1}</a>
+                <a onClick={(e) => avanzarPagina(e)} href="" className='mx-2 border-2 rounded px-5 text-amber-100 bg-black'>Next</a>
+
             </div>
 
         </section>
